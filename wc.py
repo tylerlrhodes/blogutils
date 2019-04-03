c9df300722ca327efaea13915d53d92692b5ba7b
Tyler Rhodes <tylerlrhodes@gmail.com>
2019-04-03 07:41:07 -0400

    updates..

--- wc.py
+++ wc.py
@@ -9,38 +9,62 @@
 #
 
 import sys
+import pickle
 from spellchecker import SpellChecker
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print('Please provide a file name!')
     exit(1)
 
-fn = sys.argv[1]
+if len(sys.argv) == 3:
+    add_words = bool(sys.argv[2])
+else:
+    add_words = False
+
+file_name = sys.argv[1]
 wc = 0
- 
-with open(fn) as f:
+custom_words = set()
+
+try:
+    with open('pickle.p', 'rb') as f:
+        custom_words = pickle.load(f)
+except Exception as e:
+    print(f'Error loading custom dictionary words! {e}')
+
+with open(file_name) as f:
     front_matter = True
     marker_count = 0
     spell = SpellChecker(distance=1)
+    spell.word_frequency.load_words(custom_words)
     for idx, line in enumerate(f):
         if line.strip() == '---' and front_matter == True:
             marker_count += 1
             front_matter = False if marker_count == 2 else True
             continue
         if not front_matter:
-            wc = wc + len(line.split())
+            line_words = line.split()
+            wc = wc + len(line_words)
             words = [w for w in 
                         [''.join(filter(lambda c: c.isalpha() or c == "'" or c == "-", w)) 
-                        for w in line.split()]
+                        for w in line_words]
                             if len(w) > 0]
             misspelled = spell.unknown(words)
             if len(misspelled) > 0:
                 print('\n\nFound mispelling:')
-                print(words)
+                print(f'Context: {words}')
                 for word in misspelled:
+                    custom_words.add(word)
                     print(f'{word} misspelled on {idx}, candidates are {spell.candidates(word)}')
 
 print(f'{wc} words are in the file, not including front matter')
+
+if add_words:
+    print(f'Adding words: {custom_words}')
+    try:
+        with open('pickle.p', 'wb') as f:
+            pickle.dump(custom_words, f)
+    except Exception as e:
+        print(f'There was an error saving the custom words - {e}.')
 
 
 
